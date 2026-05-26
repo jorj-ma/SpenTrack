03073f1d4e72e2aeb5480585799e410f98e761ed
George Mwanzi Anzigale <anziegeorge4@gmail.com>
2026-05-26 23:07:50 +0300

    Fix route syntax in App component

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route> path="/login" element={<Login />}</Route>
-        <Route> path="/register" element={<Register />}</Route>
+        <Route> path="/login" element={<Login />} />
+        <Route> path="/register" element={<Register />} />
         <Route path="/" element={
           <ProtectedRoute>
             <Dashboard/>
